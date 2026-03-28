49e1ec0b52a491fb2444d5c992c88095a4971d11
laporejero <laporejero1297@gmail.com>
2026-03-28 12:52:54 +0800

    Update app.js
    added a new route to fetch a single data

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ app.get('/api/products', (req, res) => {
     res.status(200).json({success:true, data: products})
 })
 
+// fetch a single data
+app.get('/api/products/:id', (req, res) => {
+    const { id } = req.params
+    const product = products.find(product => product.id === Number(id))
+
+    if (!product) {
+        return res
+            .status(404)
+            .json({
+                success: false,
+                message: 'Product not found'
+            })
+    }
+
+    res.status(200).json({
+        success: true,
+        data: product
+    })
+})
+
 const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
